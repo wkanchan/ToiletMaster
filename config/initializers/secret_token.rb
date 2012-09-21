0b8cebaf6212bc7316f6c2692e1e287ae70b948d
Knott <wkanchan@andrew.cmu.edu>
2012-09-20 23:09:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToiletMaster::Application.config.secret_token = '4b0412ac3b2cf77df7a2c5662e97764873d6ef54fb70d9c7f093a1633da68eccaf28a6b249c4c4392bf5cd88fbcd6959d79090f2b1ce5d977b8a4f04b656244e'
