3189d6d04c4172c991ac4ac8c30e754b14007cdc
Knott <wkanchan@andrew.cmu.edu>
2012-09-22 01:14:03 -0400

    Chapter 4

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,4 +1,11 @@
 class Toilet < ActiveRecord::Base
+  # this creates getters & setters
   attr_accessible :latitude, :longitude, :name
+
+  # 1 toilet has many reviews
   has_many :reviews
+
+  def url_to_google_maps
+    "http://maps.google.com/maps?q=#{self.latitude},#{self.longitude}&num=1&t=m&z=17"
+  end
 end
